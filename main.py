8a7abc777f58237c3aa108f945e396196a0bb4fd
Marcel <marcel-ju@web.de>
2022-03-25 20:53:38 +0100

    Small change

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import os
 
 print("Hello from Python!")
-print("Environment variable REPO_FULLNAME == %s"%os.getenv["REPO_FULLNAME"])
-print("os.getenv:")
-print(os.getenv)
+print("Environment variable REPO_FULLNAME == %s"%os.getenv("REPO_FULLNAME"))
