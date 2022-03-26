fb56af3156264807fecf25c4be8bcf9fbd871306
Marcel <marcel-ju@web.de>
2022-03-26 21:10:27 +0100

    Write meaningful comment

--- main.py
+++ main.py
@@ -26,8 +26,7 @@ pr_conts_final = [] #filter_candidates(candidates=pr_contributions) # TODO
 # Write similar contributions to file that can be read from the github workflow
 # and then turned into a PR comment
 output = """## Reading Recommendations
-There are many good contributions from the last years that can help as inspiration of how to create a high quality contribution yourself.
-Based on your proposal title, I have found the following outstanding previous student works that could be interesting to you:
+There are many good contributions from the last years that can help as inspiration of how to create a high quality contribution yourself. Based on your proposal title, I have found the following outstanding previous student works that could be interesting to you:
 
 """
 for c in selected_conts_final:
