2bd8cb117bdcfafd82cef53c5521b44d1b98bf64
Marcel <marcel-ju@web.de>
2022-03-26 14:48:42 +0100

    Print repo contents

--- main.py
+++ main.py
@@ -3,3 +3,5 @@ import os
 print("Hello from Python!")
 print("Environment variable REPO_FULLNAME == %s"%os.getenv("REPO_FULLNAME"))
 print("Git repo path == %s"%os.getenv("GITHUB_WORKSPACE"))
+print("Content:")
+print(os.listdir(os.getenv("GITHUB_WORKSPACE")))
